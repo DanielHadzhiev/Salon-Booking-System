7726c70acb4990cf4f0e1029c1e6ada513bcc1f0
DanielHadzhiev <danielhadziev1@gmail.com>
2023-11-21 11:16:49 +0200

    Added schedulers for tasks.

--- src/main/java/com/example/salonbookingsystem/services/NewsService.java
+++ src/main/java/com/example/salonbookingsystem/services/NewsService.java
@@ -2,7 +2,6 @@ package com.example.salonbookingsystem.services;
 
 import com.example.salonbookingsystem.model.dto.ExportNewsDTO;
 import com.example.salonbookingsystem.model.dto.ImportNewsDTO;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +14,6 @@ public interface NewsService {
 
     List<ExportNewsDTO> visualizeNews();
 
-    @Scheduled(fixedRate = 1800000)
     Map<String,String> visualizeWeather();
 
     void resetNews();
---
+++ src/main/java/com/example/salonbookingsystem/shedulers/NewsScheduler.java
@@ -0,0 +1,27 @@
+package com.example.salonbookingsystem.shedulers;
+
+import com.example.salonbookingsystem.services.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewsScheduler {
+
+    private final NewsService newsService;
+
+    @Autowired
+    public NewsScheduler(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @Scheduled(fixedRate = 43200000)
+    public void resetNewsScheduler(){
+        this.newsService.resetNews();
+    }
+
+    @Scheduled(fixedRate = 1800000)
+    public void callApiScheduler(){
+        this.newsService.visualizeWeather();
+    }
+}
